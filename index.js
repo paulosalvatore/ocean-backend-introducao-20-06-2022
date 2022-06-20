24ef6c08b051c2761e7bd190e9038475d04f5eec
Paulo Salvatore <salvatoregames@gmail.com>
2022-06-20 12:01:09 -0300

    Endpoint Read all criado

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+
+// Endpoint Read All - [GET] /herois
+app.get("/herois", function (req, res) {
+  res.send(herois);
+});
+
 app.listen(3000, () =>
   console.log("Servidor rodando em http://localhost:3000")
 );
