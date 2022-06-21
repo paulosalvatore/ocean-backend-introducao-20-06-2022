0d9ac90982908f7e8ac7d762a1a2ee80480a9a93
Paulo Salvatore <salvatoregames@gmail.com>
2022-06-21 10:47:32 -0300

    Endpoint create implementado

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+// Informar para o Express que estamos usando JSON
+// no body das requisições
+app.use(express.json());
+
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
@@ -10,6 +14,17 @@ const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
 // Endpoint Read All - [GET] /herois
 app.get("/herois", function (req, res) {
   res.send(herois);
+});
+
+// Endpoint Create - [POST] /herois
+app.post("/herois", function (req, res) {
+  // Acessa o nome do herói no corpo da requisição
+  const item = req.body.nome;
+
+  // Insere o nome na lista
+  herois.push(item);
+
+  res.send("Item inserido com sucesso!");
 });
 
 app.listen(3000, () =>
